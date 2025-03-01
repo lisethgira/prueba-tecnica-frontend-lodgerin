df5ec50b816ce102b81dfcb04ce85ff143a19c65
liseth giraldo <70545096+lisethgira@users.noreply.github.com>
2025-03-01 00:04:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# prueba-tecnica-frontend-lodgerin
+ Prueba Técnica  Frontend con React para la empresa Lodgerin
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'prueba-tecnica-frontend-lodgerin',
+      short_name: 'lodgerin',
+      description: 'prueba técnica frontend en react consumiendo API de Rick and Morty para la empresa lodgerin',
+      theme_color: '#b5da8a',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
